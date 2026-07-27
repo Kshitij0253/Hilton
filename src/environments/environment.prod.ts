99503126adf68436ceee373688629a0316a01048
Kshitij0253 <53930695+Kshitij0253@users.noreply.github.com>
2026-07-27 22:21:54 +0530

    Update environment.prod.ts
    changes done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://spring-hilton.onrender.com/api/contact/submit'
+  // apiUrl: 'https://spring-hilton.onrender.com/api/contact/submit'
+
+   apiUrl: 'http://13.201.40.59:8080/api/contact/submit'
 };
